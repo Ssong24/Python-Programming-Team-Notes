0934391cd19aa3935b1005f30c99f8fac0264385
Ssong24 <pub50@naver.com>
2021-10-26 14:47:54 +0900

    Update dijkstra_shortest_path.py

--- Graph/dijkstra_shortest_path.py
+++ Graph/dijkstra_shortest_path.py
@@ -29,9 +29,9 @@ def dijkstra(graph, start, distance):
         # 현재 노드가 이미 처리된 노드라면 무시 (코드적 의미는 이전에 업데이트 거리가 더 작으면 그냥 패쓰)
         if distance[now] < dist:
             continue
-        # 현재 노드와 연결된 다른 인접한 노드들읋 확인
+        # 현재 노드와 연결된 다른 인접한 노드들을 확인
         for i in graph[now]:
-            cost = dist + i[1]
+            cost = dist + i[1]  # i[1]:연결된 인접 노드까지의 비용
             # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
             if cost < distance[i[0]]:
                 distance[i[0]] = cost
@@ -43,6 +43,8 @@ start_time = time.time()
 dijkstra(graph, start, distance)
 end_time = time.time()
 print('Time: {:5f}'.format(end_time - start_time))  # 0.00000
+
+
 # 모든 노드로 가기 위한 최단 거리를 출력
 for i in range(1, n+1):
     # 도달할 수 없는 경우, 무한(INFINITY)이라고 출력
